c26cb00724961c12edb9b6dd80ac616271a58f80
Nyxxie <nyxxxxie@gmail.com>
2018-02-22 23:53:02 -0500

    Added ssh directory walking.

--- scanners/ssh_keys.go
+++ scanners/ssh_keys.go
@@ -2,15 +2,32 @@ package scanners
 
 import (
 	"../scan_manager"
+	"os"
 	"fmt"
+	"path/filepath"
 )
 
 type SSHKeyScanner struct{}
 
+func HandleFile(path string, f os.FileInfo, err error) error {
+	if !f.IsDir() {
+		if f.Name() == "known_hosts" {
+			fmt.Println("Known hosts: " + path)
+		} else if f.Name() == "authorized_keys" {
+			fmt.Println("Authorized keys: " + path)
+		} else if f.Name() == "config" {
+			fmt.Println("Config: " + path)
+		} else {
+			fmt.Println("Key: " + path)
+		}
+	}
+	return nil
+}
+
 func HandleDirectory(obj *scan_manager.DirScanObject) {
 	info := obj.FileInfo();
 	if info.Name() == ".ssh" {
-		fmt.Printf("%s    %s    %s\n", info.ModTime(), info.Mode(), obj.Path());
+		filepath.Walk(obj.Path(), HandleFile)
 	}
 }
 
