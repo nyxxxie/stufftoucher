8126589dc16188b477ffa6030359eb4808d4b81b
Nyxxie <nyxxxxie@gmail.com>
2018-03-12 03:02:42 -0400

    Added some todos.

--- scanners/firefox.go
+++ scanners/firefox.go
@@ -10,6 +10,10 @@ type FirefoxScanner struct{}
 
 func (scanner *FirefoxScanner) HandleFile(obj *scan_manager.FileScanObject) {
 	if strings.Contains(obj.Path(), "key3.db") {
+		// TODO: figure out if key3.db is a firefox database
+		// TODO: figure out if key3.db contains the master key or w/e
+		// TODO: figure out how to get logins from key3 and other files.
+		// TODO: print out logins or the file contents if an option is enabled and if we're unable to get the password for the keystore
 		fmt.Println(obj.Path())
 	}
 }
